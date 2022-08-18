821b52b558574f9e7844f2ff4895326f3f68bb44
= <=>
2022-08-18 23:27:44 +0530

    Initial commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecommerce</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/master.css')}}" type="text/css">
+    <script src="{{asset('js/bootstrap.js')}}"></script>
+</head>
+<body>
+    {{ View::make('header')}}
+    
+    @yield('content')
+
+    {{ View::make('footer')}}
+
+</body>
+</html>
